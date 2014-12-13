5af722e1f45fc7d869d1acf9579fdc0b983b7d7d
GeoNomad <pjennings@gmail.com>
2014-12-13 09:52:55 -0800

    Latest Version
    tmr.stop context menu lists timers to stop
    
    Upload file list automatically populated

--- version 0.65/LuaLoader.lua
+++ version 0.65/LuaLoader.lua
@@ -1,12 +1,16 @@
 -- LuaLoader Utility Functions
 -- add dofile('LuaLoader.lua') to your init.lua
 
+cat = nil
 function cat( fname )
    file.open( fname, "r")
+   local count = 0
    while true do
       local line = file.readline()
       if (line == nil) then break end
-      print(string.sub(line, 1, -2)) 
+      count = count+1
+      print(string.format("%3d", count)..':'..string.sub(line, 1, -2)) 
+      tmr.wdclr()
       end
    file.close()
 end
